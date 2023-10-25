85d7f766fa3d080e8b4e9fc403112b4c004c1a4b
Ethan Chen <79673816+ethan-l-chen-24@users.noreply.github.com>
2023-10-25 14:07:42 -0400

    working signup and login authentication (#41)

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,12 +1,37 @@
 import { Link, Form } from '@remix-run/react';
+import { redirect } from '@remix-run/node';
 import loginStyle from '~/styles/home.css'
+import axios from 'axios';
+
+export async function action({
+	request,
+  }: ActionFunctionArgs) {
+	const body = await request.formData();
+
+	var myJson = {};
+	for(const [key, value] of body.entries()) {
+		myJson[key] = value;
+	}
+
+	console.log(JSON.stringify(myJson));
+
+	try {
+		const response = await axios.post('http://opportune_backend:3000/auth/login', myJson);
+
+	} catch(error) {
+		console.log(error)
+		return null
+	}
+	
+	return redirect(`/dashboard`);
+  }
 
 export default function Login() {
   return (
     <main id="block-container">
       <h1>Opportune</h1>
       <p>Tuning the opportunities you will have at your company to the maximum.</p>
-	  <Form method="post" id="login">
+	  <Form method="post" action="/login" id="login">
 		<p>
 			<label htmlFor="username">Username</label>
 			<input type="text" id="username" name="username" required />
@@ -16,7 +41,7 @@ export default function Login() {
 			<input type="password" name="password" required />
 		</p>
 		<div className="form-actions">
-			<button> {'Login'} </button>
+			<button type="submit"> {'Login'} </button>
 		</div>
 		<p className="cta">
         	<Link to="/dashboard">Login</Link>
