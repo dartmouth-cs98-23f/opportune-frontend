5acfb1f8d8e52d9e03d72f7f2d78d37005568e47
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2023-10-25 13:34:45 -0400

    working signup and login authentication

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,11 +1,41 @@
-import { Link, Form} from '@remix-run/react';
-import loginStyle from '~/styles/home.css'
+import { Link, Form } from '@remix-run/react';
+import { redirect } from '@remix-run/node';
+import loginStyle from '~/styles/home.css';
+import axios from 'axios';
+
+export async function action({
+	request,
+  }: ActionFunctionArgs) {
+	const body = await request.formData();
+
+	var myJson = {};
+	for(const [key, value] of body.entries()) {
+		myJson[key] = value;
+	}
+
+	console.log(JSON.stringify(myJson));
+
+	try {
+		const response = await axios.post('http://opportune_backend:3000/auth/register', myJson);
+
+		if(response.status == 204) {
+			console.log("Username is already in use.")
+			return null
+		}
+
+	} catch(error) {
+		console.log(error)
+	}
+	
+	return redirect(`/login`);
+  }
 
 export default function SignUp() {
+
   return (
     <div id="block-container">
       	<h1>Opportune</h1>
-      	<Form method="post" id="login">
+      	<Form method="post" action="/signup" id="login">
 			<p>
 				<label htmlFor="username">Pick a username: </label>
 				<input type="text" id="username" name="username" required />
@@ -14,10 +44,8 @@ export default function SignUp() {
 				<label htmlFor="password">Pick a strong password: </label>
 				<input type="password" name="password" required />
 			</p>	
+			<button type="submit">Submit</button>
 	  	</Form>
-		<p className="cta">
-			<Link to="/login">Confirm</Link>
-		</p>
 		<p>Already have an account? <Link to="/login">Sign in</Link></p>
     </div>
   );
