dbbb5bec1d2858c5b3eff979cba1d934429e3e4f
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2023-11-11 17:38:24 -0500

    added team card

--- app/routes/results.tsx
+++ app/routes/results.tsx
@@ -3,6 +3,8 @@ import styles from '~/styles/home.css';
 import MainNavigation from '~/components/MainNav';
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
 import axios from 'axios';
+import { useState } from 'react';
+import { useCalendlyEventListener, InlineWidget, PopupButton, PopupModal } from "react-calendly";
 import { ActionFunctionArgs, LoaderFunctionArgs, json, redirect } from '@remix-run/node';
 import { destroySession, getSession } from '~/utils/sessions';
 // import { motion } from 'framer-motion';
@@ -53,8 +55,6 @@ export async function loader({request}: LoaderFunctionArgs) {
 			const data = response.data;
 			console.log(data);
 			return json({ data });
-		} else if (response.status === 404) {
-			return null;
 		}
 	} catch (error) {
 		console.log(error);
@@ -91,8 +91,70 @@ export default function Results() {
 }
 
 export function matchingResults(teamInfo: json) {
+	console.log("Team Info: " + teamInfo);
+
 	if(teamInfo) {
-		return (<p>Congrats! You were matched to {teamInfo.data.team.name}</p>)
+		
+		const team = teamInfo.data.team;
+		const [expanded, setExpanded] = useState(false);
+		const [isOpen, setIsOpen] = useState(false);
+
+		const prefill={
+			email: 'test@test.com',
+			firstName: 'Jon',
+			lastName: 'Snow',
+			name: 'Jon Snow',
+		}
+
+		return (
+			<div>
+				<p>Congrats, we found you a team!</p>
+				<p>We think you'd be an excellent match.</p>
+				<div className="team-box" key={team.name}>
+					<div className="team-text">
+						<div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+							<h3>{team.name}</h3>
+						</div>
+						
+						<p> 
+							<b>Tools and Technologies: </b>
+							{team.skills.map(skills => skills.name).join(", ")}
+						</p>
+						<p className='read-more-btn' onClick={() => setExpanded(!expanded)}>
+							{expanded ? 'Read Less' : 'Read More'}  
+						</p>
+
+						{expanded && 
+							<div className="expanded-content">
+								<div className="text-content">
+									<p>Description: {team.description}</p>
+								</div>
+
+								<button
+									className='schedule-btn'
+									onClick={() => setIsOpen(true)}
+									type='button'>
+									Schedule meeting
+								</button>
+								{isOpen && 
+								<div className='calendly-overlay'>
+									<div className='calendly-close-overlay' onClick={() => setIsOpen(false)}></div>
+									<div className='calendly-popup'>
+										<div className='calendly-popup-content'>
+											<InlineWidget url="https://calendly.com/ryanl23" 
+												prefill={prefill}
+											/>
+										</div>
+									</div>
+								</div>
+								}
+
+							</div>
+						}
+					</div>
+				</div>
+			</div>
+		)
 	} else {
 		return (<p>Matching results will be out on July 2.</p>)
 	}
