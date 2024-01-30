007044ccf8da9fe1735aeccdfe116f97616af7cd
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2024-01-30 16:47:26 -0500

    wiring for demo

--- app/routes/companyMatching.tsx
+++ app/routes/companyMatching.tsx
@@ -1,11 +1,123 @@
-import { Form, useLoaderData } from '@remix-run/react';
+import { Link, Form, useLoaderData } from '@remix-run/react';
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
 import styles from '~/styles/home.css'
 import { useState } from 'react';
+import axios from 'axios';
 import ImageUpload from '~/components/ImageUpload';
+import { ActionFunctionArgs, LoaderFunctionArgs, redirect, json } from '@remix-run/node';
 import ReadMore from '~/components/ReadMore';
+import { destroySession, getSession } from '../utils/sessions';
+
+export async function action({request}: ActionFunctionArgs) {
+	const body = await request.formData();
+	const _action = body.get("_action");
+
+	const session = await getSession(
+		request.headers.get("Cookie")
+	);
+
+	var myJson = {};
+	for (const [key, value] of body.entries()) {
+		myJson[key] = value;
+	}
+
+	console.log("My json: ", JSON.stringify(myJson));
+
+	if (_action === "LogOut") {
+		return redirect("/login", {
+			headers: {
+			  "Set-Cookie": await destroySession(session),
+			},
+		});
+	} else if (_action === "matchingSurvey") {
+		try {
+      console.log("Auth: " + session.get("auth"));
+
+			const response = await axios.post(process.env.BACKEND_URL + '/api/v1/company/match', myJson, {
+				headers: {
+				"Authorization": session.get("auth"),
+				"Content-Type": "application/json",
+				},
+			})
+			return redirect("/companyMatching");
+		} catch (error) {
+			console.log(error);
+			return null;
+		}
+	} else {
+  		return null;
+  }
+}
+
+export async function loader({request}: LoaderFunctionArgs) {
+  try {
+    const session = await getSession(
+			request.headers.get("Cookie")
+		);
+
+    console.log("Auth: ", session.get("auth"));
+
+    if(!session.get("auth")) {
+      return redirect("/login");
+    }
+
+     const companyRes = await axios.get(process.env.BACKEND_URL + '/api/v1/company/profile', {
+       headers: {
+         "Authorization": session.get("auth"),
+         "Content-Type": "application/json",
+       }
+     });
+
+    const newHireRes = await axios.get(process.env.BACKEND_URL + '/api/v1/user/list-newhires', {
+      headers: {
+        "Authorization": session.get("auth"),
+        "Content-Type": "application/json",
+      }  
+    });
+
+    const teamsRes = await axios.get(process.env.BACKEND_URL + '/api/v1/user/list-teams', {
+      headers: {
+        "Authorization": session.get("auth"),
+        "Content-Type": "application/json",
+      }
+    });
+
+    if (newHireRes.status === 200 && teamsRes.status === 200 && companyRes.status == 200) {
+			const newHires = newHireRes.data;
+			const teams = teamsRes.data;
+      const data = companyRes.data;
+			console.log("Loader data: ", newHires);
+			return json({ data, newHires, teams });
+		}
+
+  } catch (error) {
+    console.log(error);
+    return null; 
+  }
+
+}
 
 export default function CompanyMatching() {
+
+  const info = useLoaderData<typeof loader>();
+
+  var allSurveysCompleted = true;
+  var teamIdMap = {};
+  var nhIdMap = {};
+  for(var team of info.teams.teams) {
+    teamIdMap[team._id] = team;
+    if(team.survey_complete == false) {
+      allSurveysCompleted = false;
+    }
+  }
+  for(var nh of info.newHires.new_hires) {
+    nhIdMap[nh._id] = nh;
+    if(!allSurveysCompleted) break;
+    if(nh.survey_complete == false) {
+      allSurveysCompleted = false;
+    }
+  }
+
   const [teams, setTeams] = useState([
     {  
       name: 'Team A',
@@ -120,15 +232,15 @@ export default function CompanyMatching() {
             <h2>Teams</h2>
           </div>
           <div className="teams-list">
-            {teams.map(team => (
+            {info.teams.teams.map(team => (
               <div key={team.name} className='company-team'>
                 <div>
                   <h3>{team.name}</h3> 
-                  <p>{team.org}</p>
-                  <div>{team.location}</div>
+              {/*   <p>{team.org}</p>   
+                  <div>{team.location}</div> */}
                 </div>
                 <ReadMore text={team.description}></ReadMore>
-                <button className={team.formFilled ? 'done-button' : 'in-progress-button'}>{team.formFilled ? 'Done' : 'In Progress'}</button>
+                <button className={team.survey_complete ? 'done-button' : 'in-progress-button'}>{team.survey_complete ? 'Done' : 'In Progress'}</button>
                 <button className="newhire-button">
                   Email nudge
                 </button>
@@ -142,16 +254,16 @@ export default function CompanyMatching() {
             <h2>New Hire</h2>
           </div>
           <div className="teams-list">
-            {newHires.map(newHire => (
+            {info.newHires.new_hires.map(newHire => (
               <div key={newHire.name} className='company-team'>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
-                  <h3>{newHire.name}</h3> 
+                  <h3>{newHire.first_name} {newHire.last_name}</h3> 
                   <p>{newHire.email}</p>
-                  <p>Unmatched</p>
+                  <p>{newHire.team_id ? teamIdMap[newHire.team_id].name : "Unmatched"}</p>
                 </div>
-                <button className={newHire.formFilled ? 'done-button' : 'in-progress-button'}>{newHire.formFilled ? 'Done' : 'Incomplete'}</button>
+                <button className={newHire.survey_complete ? 'done-button' : 'in-progress-button'}>{newHire.survey_complete ? 'Done' : 'Incomplete'}</button>
                 <select>
-                  {teams.map(team => (
+                  {info.teams.teams.map(team => (
                     <option key={team.name} value={team.name}>
                       {team.name}
                     </option>
@@ -164,9 +276,17 @@ export default function CompanyMatching() {
       </div>
       
       <p className="cta" style={{textAlign: 'right'}}>
-        <button type="submit" name="_action" value="matchingSurvey">
-          Run matching survey
-        </button>
+        
+        {allSurveysCompleted ? 
+        <Form action="/companyMatching" method="post">
+          <button type="submit" name="_action" value="matchingSurvey">
+            Run matching survey
+          </button> 
+        </Form>
+        
+        :
+        <></>}
+        
       </p>
     </div>
   );
