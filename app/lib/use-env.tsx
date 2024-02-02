83e45ba890db1ad87b42c9ba35cfc438c1f84107
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2024-02-02 16:30:37 -0500

    fixed all routing for user types

--- app/lib/use-env.tsx
+++
@@ -1,9 +0,0 @@
-import { useMatches } from '@remix-run/react'
-
-export const useEnv = () => {
-  const matches = useMatches()
-  const { ENV } = (matches.find((route) => {
-    return route.id === 'root'
-  })?.data || {})
-  return ENV || {}
-}
