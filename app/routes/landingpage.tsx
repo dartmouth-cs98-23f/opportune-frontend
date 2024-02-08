9cfa1f34036ac5df69291c719ebf47d18d7cbec6
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2024-02-08 15:34:16 -0500

    added filler form for signup at very bottom

--- app/routes/landingpage.tsx
+++ app/routes/landingpage.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, useRef } from 'react';
-import { Link } from '@remix-run/react';
+import { json, redirect, ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+import { Link, Form } from '@remix-run/react';
 import { useInView } from 'react-intersection-observer';
 
 interface AnimatedSectionProps {
@@ -21,6 +22,27 @@ interface AnimatedSectionProps {
     );
   }
 
+
+  export async function action({
+	request,
+  }: ActionFunctionArgs) {
+	const body = await request.formData();
+
+	var myJson = {};
+	for (const [key, value] of body.entries()) {
+		myJson[key] = value;
+	}
+
+	console.log(JSON.stringify(myJson));
+
+	try {
+		//const response = await axios.post(process.env.BACKEND_URL + '/api/v1/auth/login', myJson);
+
+		return null;
+	} catch(error) {
+		return null;
+	}
+};
 
 
 
@@ -222,39 +244,22 @@ function LandingPage() {
                                 <!-- To make this form functional, sign up at-->
                                 <!-- https://startbootstrap.com/solution/contact-forms-->
                                 <!-- to get an API token!--> */}
-                                <form className="form-subscribe" id="contactFormFooter" data-sb-form-api-token="API_TOKEN">
-                                    {/* <!-- Email address input--> */}
+                                <Form className="fomr-subscribe" id="contactFormFooter" method="post" action="/landingpage">
                                     <div className="row">
                                         <div className="col">
-                                            <input className="form-control form-control-lg" id="emailAddressBelow" type="email" placeholder="Email Address" data-sb-validations="required,email" />
-                                            <div className="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:required">Email Address is required.</div>
-                                            <div className="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:email">Email Address Email is not valid.</div>
+                                            <input className="form-control form-control-lg" id="emailAddressBelow" type="email" placeholder="Email Address" name="email" />
+                                            {/*<div className="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:required">Email Address is required.</div>
+                                            <div className="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:email">Email Address Email is not valid.</div>*/}
                                         </div>
-                                        <div className="col-auto"><button className="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                                        <div className="col-auto"><button className="btn btn-primary btn-lg" id="submitButton" type="submit">Submit</button></div>
                                     </div>
-                                    {/* <!-- Submit success message-->
-                                    <!---->
-                                    <!-- This is what your users will see when the form-->
-                                    <!-- has successfully submitted--> */}
-                                    <div className="d-none" id="submitSuccessMessage">
-                                        <div className="text-center mb-3">
-                                            <div className="fw-bolder">Form submission successful!</div>
-                                            <p>To activate this form, sign up at</p>
-                                            <a className="text-white" href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                        </div>
-                                    </div>
-                                    {/* <!-- Submit error message-->
-                                    <!---->
-                                    <!-- This is what your users will see when there is-->
-                                    <!-- an error submitting the form--> */}
-                                    <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
-                                </form>
+                                </Form>
                             </div>
                         </div>
                     </div>
                 </section>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-                <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+                {/*<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+                <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>*/}
             </body>
         </html>
     );
