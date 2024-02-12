ac396eabef5b48750e530537ceefe222f3196be7
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2024-02-12 16:36:15 -0500

    added messages saying when the deadline is - needs css

--- app/components/survey_qs/Progress.tsx
+++
@@ -1,9 +0,0 @@
-interface Percent {
-	pct: number;
-}
-
-export default function Progress(props:Percent) {
-	return <div className="progress-container">
-		     <div id="progress" style={{width: `${props.pct}%`}}></div>
-		   </div>;
-}
