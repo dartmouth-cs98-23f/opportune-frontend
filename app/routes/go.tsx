7ae34a2b694d54ad4f449ba3f3aef6b1dd28cb17
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2024-02-21 14:42:02 -0500

    go page

--- app/routes/go.tsx
+++ app/routes/go.tsx
@@ -1,17 +1,19 @@
 import { Link } from '@remix-run/react';
 
-export default function Index() {
+export default function Go() {
   return (
     <div className="block-container">
-		<div className="landing-box">
-			<img className="opportune-logo-large" src="opportune_newlogo.svg"></img>
-			<h1>Opportune</h1>
-			<p>Tuning the opportunities you will have at your company to the maximum.</p>
-			<p className="cta">
-				<Link to="/signup">New Hires</Link>
-				<Link to="/tsignup">Teams</Link>
-			</p>
-		</div>
+      <div className="landing-box">
+        <img className="opportune-logo-large" src="opportune_newlogo.svg"></img>
+        <h1>Opportune</h1>
+        <p>
+          Tuning the opportunities you will have at your company to the maximum.
+        </p>
+        <p className="cta">
+          <Link to="/login">Login</Link>
+          <Link to="/company/signup">Enroll Your Company</Link>
+        </p>
+      </div>
     </div>
   );
 }
