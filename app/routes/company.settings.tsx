13569157060a8d082ecfd633faa61cc5b015844d
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2024-03-03 19:57:03 -0500

    added styling and failstates

--- app/routes/company.settings.tsx
+++ app/routes/company.settings.tsx
@@ -1,9 +1,13 @@
 import { Link, Form, useLoaderData, useLocation } from '@remix-run/react';
 import { json, redirect, ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import TextField from '~/components/TextField';
+import styles from '~/styles/home.css';
 import axios from 'axios';
 import { getSession, commitSession, destroySession } from "../utils/sessions";
 import TRDropdown from '~/components/TRDropdown';
+import {
+  InformationCircleIcon,
+} from '@heroicons/react/24/outline';
 
 // LOADER FUNCTION
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -72,7 +76,7 @@ export async function action({
             });
         } catch (error) {
             console.log(error);
-            return null;
+            return redirect('/company/settings?failed=1');
         }
     } else if(_action === 'changePassword') {
         try {
@@ -93,7 +97,7 @@ export async function action({
             });
           } catch (error) {
             console.log(error);
-            return null;
+            return redirect('/company/settings?failed=2');
           }
     } else {
         return null;
@@ -105,6 +109,30 @@ const navLabels = ["Profile", "Matching", "Settings"]
 export default function CompanySettings() {
     const info = useLoaderData<typeof loader>();
 
+    const location = useLocation();
+
+    // Access query parameters from location.search
+    const queryParams = new URLSearchParams(location.search);
+    const failstate = queryParams.get('failed');
+
+    const renderFailState = () => {
+      if(failstate == 1) {
+        return (
+          <p className='failed-auth' style={{margin: "0 2rem"}}>
+            The email change failed. Please ensure you have entered a valid email that is not currently in use, then try again.
+          </p>
+        )
+      } else if(failstate == 2) {
+        return (
+          <p className='failed-auth' style={{margin: "0 2rem"}}>
+            The password change failed. Please ensure you have entered  the correct password.
+          </p>
+        )
+      } else {
+        return null;
+      }
+      }
+
     return (
         <div className="company-container">
             <div id="sidebar">
@@ -112,37 +140,57 @@ export default function CompanySettings() {
                 <p className="text-logo">Opportune</p>
                 <TRDropdown labels={navLabels} route="/company/profile" userType="company" />
             </div>
-            <div>Change Email: </div>
-                <Form method="post" action="/company/settings">
-                    <TextField
-                        label="Email"
-                        classLabel="email"
-                        value={info?.data.company.email}
-                        type="email"
-                    />
-                    <div className="form-actions">
-                        <button type="submit" name="_action" value="changeEmail">Save</button>
-                    </div>
-                </Form>
+            <h1 style={{margin: "2rem 2rem"}}>Settings</h1>
+            {renderFailState()}
+            <h2 style={{margin: "2rem 2rem"}}>Change Email: </h2>
+            <Form method="post" action="/company/settings" className="info-form form-left">
+              <div className="matching-unavailable" style={{"justify-content": "flex-start"}}>
+                <InformationCircleIcon className="lock-icon" />
+                <p style={{ marginLeft: '0.5rem' }}>
+                  This action will log you out.
+                </p>
+              </div>
+              <TextField
+                  label="Email"
+                  classLabel="email"
+                  value={info?.data.company.email}
+                  type="email"
+              />
+              <div className="form-actions">
+                  <button type="submit" name="_action" value="changeEmail">Save</button>
+              </div>
+            </Form>
 
-                <div>Change Password: </div>
-                <Form method="post" action="/company/settings">
-                    <TextField
-                        label="Old Password"
-                        classLabel="old_password"
-                        value=""
-                        type="password"
-                    />
-                    <TextField
-                        label="New Password"
-                        classLabel="new_password"
-                        value=""
-                        type="password"
-                    />
-                    <div className="form-actions">
-                        <button type="submit" name="_action" value="changePassword">Save</button>
-                    </div>
-                </Form>
+            <h2 style={{margin: "2rem 2rem"}}>Change Password: </h2>
+            <Form method="post" action="/company/settings" className="info-form form-left">
+              <div className="matching-unavailable" style={{"justify-content": "flex-start"}}>
+                <InformationCircleIcon className="lock-icon" />
+                <p style={{ marginLeft: '0.5rem' }}>
+                  This action will log you out.
+                </p>
+              </div>
+              <TextField
+                  label="Old Password"
+                  classLabel="old_password"
+                  value=""
+                  type="password"
+              />
+              <TextField
+                  label="New Password"
+                  classLabel="new_password"
+                  value=""
+                  type="password"
+              />
+              <div className="form-actions">
+                  <button type="submit" name="_action" value="changePassword">Save</button>
+              </div>
+            </Form>
         </div>
       );
+}
+
+export function links() {
+  return [
+    { rel: 'stylesheet', href: styles },
+  ];
 }
