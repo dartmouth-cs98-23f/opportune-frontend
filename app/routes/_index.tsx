e01c7c20d928601868dce25f4b649a150431ae17
Stephen Wang <stephen.wang.24@dartmouth.edu>
2024-03-05 14:38:06 -0500

    add demo video

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -76,7 +76,7 @@ function HowWeWorkPage() {
                             </p>
                             <div className="d-flex justify-content-center justify-content-lg-start">
                                 <Link className="btn-get-started" to="/go">Get Started</Link>
-                                <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>Watch Video</span></a>
+                                <a href="https://www.youtube.com/watch?v=dAz8cWilSGI" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>Watch Video</span></a>
                             </div>
                         </div>
                         <div className="col-lg-6 order-1 order-lg-2">
